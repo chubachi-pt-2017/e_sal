b57d1cb9bf38412d1e58d324b76336d8136f184e
Kensuke <indy0214@outlook.com>
2017-07-02 00:33:57 +0000

    ログイン後のトップページデザインを作成中

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :e_sal, :path => "e-sal" do
+     get '/' => 'homes#index'
+  end
 end
