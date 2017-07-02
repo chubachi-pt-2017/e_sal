0e788d741d1a5f8d9bb00d0fd807be6c7cad1fbe
Kensuke <indy0214@outlook.com>
2017-07-02 00:39:12 +0000

    ログイン後のトップページデザイン作成中

---
+++ spec/helpers/service/homes_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the Service::HomesHelper. For example:
+#
+# describe Service::HomesHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe Service::HomesHelper, type: :helper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
