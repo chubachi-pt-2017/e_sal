d8e90c1a1b515f503f5f1ef8fd82e964e6ad1ac0
Kensuke <indy0214@outlook.com>
2017-07-16 07:50:52 +0000

    コメント許可する/しないのselectボックス作成

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -4,10 +4,22 @@ class Tutorial < ApplicationRecord
     DRAFT = 1
     PUBLISHED = 2
 
-    def self.select_options
+    def self.publish_select_options
       {
         "非公開" => DRAFT,
         "公開" => PUBLISHED
+      }
+    end
+  end
+
+  module CommentPermission
+    NG = 1
+    OK = 2
+
+    def self.comment_select_options
+      {
+        "許可しない" => NG,
+        "許可する" => OK
       }
     end
   end
@@ -18,12 +30,21 @@ class Tutorial < ApplicationRecord
   belongs_to :users
   accepts_nested_attributes_for :tutorial_content, allow_destroy: true
 
-  def tutorial_status
+  def check_status
     return Status::PUBLISHED if published?
     Status::DRAFT
   end
 
   def published?
     status == Status::PUBLISHED
+  end
+
+  def check_comment_permission
+    return CommentPermission::OK if approve_comment?
+    CommentPermission::NG
+  end
+
+  def approve_comment?
+    comment_enable_flg == CommentPermission::OK
   end
 end
