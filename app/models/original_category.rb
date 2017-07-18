2759be84c1163e828edfc8c51e36dedeaed5d8b7
Kensuke <indy0214@outlook.com>
2017-07-18 14:44:04 +0000

    カテゴリの名前とurlに長さ制限を追加

--- app/models/original_category.rb
+++ app/models/original_category.rb
@@ -4,12 +4,14 @@ class OriginalCategory < ApplicationRecord
 
   validates :name,
   presence: { message: "カテゴリ名(表示用)を入力してください。"},
-  uniqueness: { message: "入力したカテゴリ名(表示用)は既に存在しています。"}
+  uniqueness: { message: "入力したカテゴリ名(表示用)は既に存在しています。"},
+  length: { maximum: 30, message: "最大文字数は30文字です。" }
 
   validates :name_url,
   presence: { message: "カテゴリ英語名を入力してください。"},
   uniqueness: { message: "入力したカテゴリ英語名は既に存在しています。"},
-  format: { with: /\A[0-9a-z-]+\z/, message: "半角の小文字/数字とハイフンのみが使用できます。" }
+  format: { with: /\A[0-9a-z-]+\z/, message: "半角の小文字/数字とハイフンのみが使用できます。" },
+  length: { maximum: 40, message: "最大文字数は40文字です。" }
 
   validates :main_category_id,
   presence: { message: "メインカテゴリを選択してください。"}
