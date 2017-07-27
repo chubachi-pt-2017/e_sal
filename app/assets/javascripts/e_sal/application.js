2f4a435e7f4a27d464a8d55d39ec9ac233e8ca76
Kensuke <indy0214@outlook.com>
2017-07-27 17:18:24 +0000

    右上のメニュー作成

--- app/assets/javascripts/e_sal/application.js
+++ app/assets/javascripts/e_sal/application.js
@@ -2,3 +2,9 @@
 //= require jquery_ujs
 //= require moment
 //= require bootstrap-datetimepicker
+
+$(function(){
+  $("#js-header-menu").on("click", function(){
+    $("#js-header-menu-list").toggle();
+  });
+})
